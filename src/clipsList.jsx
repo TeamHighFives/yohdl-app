367729e8fa2b332ae46f7f3b7687bf818fc2ac69
Michael Kulinski <me@michaelkulinski.com>
2016-09-15 16:37:54 -0700

    final pull requesting

--- src/clipsList.jsx
+++ src/clipsList.jsx
@@ -64,9 +64,11 @@ class ClipsList extends Component {
     });
   }
     return (
-      <div className="right">
-        <div onClick={this.playAll}>Play all</div>
-        <div onClick={this.stopPlayAll}>Stop all</div>
+      <div>
+        <div>
+          <button onClick={this.playAll}>Play all</button>
+          <button onClick={this.stopPlayAll}>Stop all</button>
+        </div>
         <span id="clips">
           <ul>
             {items}
